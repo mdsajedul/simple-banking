06b3974e6bc9db9d4bf7db279cc292320c40f3e0
mdsajedul <sajedulislms@gmail.com>
2021-08-15 15:31:21 +0600

    login change

--- js/login.js
+++ js/login.js
@@ -6,12 +6,22 @@ document.getElementById('login-submit').addEventListener('click',function(){
     const passwordField = document.getElementById('user-password');
     const userPassword = passwordField.value;
 
-    if(userEmail=='sontan@gmail.com' && userPassword == 'secret'){
-        console.log('valid user ')
-        window.location.href = 'banking.html';
+    // if(userEmail=='sontan@gmail.com' && userPassword == 'secret'){
+        
+    //     window.location.href = 'banking.html';
+    // }
+    // else{
+        
+    // }
+    
+    if(userEmail=='sontan@gmail.com'){
+        if(userPassword=='secret'){
+            window.location.href = 'banking.html'
+        }
     }
     else{
-        console.log('invalid user ')
+        emailField.value = 'Invalid user'
     }
-    
+
+
 })
